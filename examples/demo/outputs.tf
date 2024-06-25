af9ca6e1aff4bbfba6860f83e5068b93f978afbf
James McGuire <5710033+jmcguire23@users.noreply.github.com>
2024-06-25 14:04:55 +0100

    fix: add user vars to demo

--- examples/demo/outputs.tf
+++ examples/demo/outputs.tf
@@ -7,6 +7,31 @@ output "deployment" {
   description = "Deployment UID"
 }
 
+output "web_hostname" {
+  value       = module.queue_manager.web_console_url
+  description = "Web Console URL"
+}
+
+output "rest_endpoint" {
+  value       = module.queue_manager.rest_api_endpoint_url
+  description = "REST API Endpoint"
+}
+
+output "admin_api_endpoint" {
+  value = module.queue_manager.administrator_api_endpoint_url
+  description = "Administrator api endpoint url"
+}
+
+output "connection_info_uri" {
+  value = module.queue_manager.connection_info_uri
+  description = "URI to fetch connection information for queue manager"
+}
+
+output "qmgr_id" {
+  value       = module.queue_manager.id
+  description = "Queue Manager ID"
+}
+
 output "location" {
   value       = module.mqcloud_instance.queue_manager_options.locations[0]
   description = "First location available on the MQ on Cloud deployment"
